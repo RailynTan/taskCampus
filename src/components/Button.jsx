756ffe6d5754c910f34ca192b3d9d8ecc84c8fcf
Pau404 <pablourzua09@gmail.com>
2026-05-25 00:57:48 -0400

    Button jsx

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+const Button = ({ label, onClick, variant = "primary", disabled = false}) => {
+    return (
+        <button
+        className={'btn btn${variant}'}
+        onClick={onClick}
+        disabled={disabled}>
+         {label}
+        </button>
+    );
+};
+
+export default Button
